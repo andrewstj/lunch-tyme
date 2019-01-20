1a126c53895af765da4b997d8f8978fa6ed4cd8d
andrewstj <tj@andrewsrealm.net>
2019-01-20 11:11:51 -0600

    Refactor and update homepage

--- src/map-item.tsx
+++ src/map-item.tsx
@@ -7,20 +7,17 @@ interface Props {
   lng: number;
 }
 
-const K_WIDTH = 10;
-const K_HEIGHT = 10;
+const RADIUS = 10;
 
 const mapPointStyle: CSSProperties = {
-  // initially any map object has left top corner at lat lng coordinates
-  // it's on you to set object origin to 0,0 coordinates
   position: "absolute",
-  width: K_WIDTH,
-  height: K_HEIGHT,
-  left: -K_WIDTH / 2,
-  top: -K_HEIGHT / 2,
+  width: RADIUS,
+  height: RADIUS,
+  left: -RADIUS / 2,
+  top: -RADIUS / 2,
 
   border: "1px solid white",
-  borderRadius: K_HEIGHT,
+  borderRadius: RADIUS,
   backgroundColor: "#0000b5",
   padding: 4
 };
